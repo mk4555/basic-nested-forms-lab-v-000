695286f785800a97211965ff4836f3f83b10660f
Min Kim <mk4555@nyu.edu>
2018-02-17 00:20:03 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  def ingredient_attributes=(ingredient)
+
+  end
 end
